79a4c727947bd6b8f2c0e6bc52d4e9725f0835fe
suresh32332 <sureshsuresh32332@gmail.com>
2025-06-27 08:40:47 +0530

    add count but not changed

--- VotingSystem.java
+++ VotingSystem.java
@@ -1,13 +1,14 @@
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.*;
+
 public class VotingSystem {
-        static Map<String, Integer> candidate = new HashMap<>();
-        static Set<String> voted = new HashSet<>();
-         static Scanner sc = new Scanner(System.in);
+    static Map<String, Integer> candidate = new HashMap<>();
+    static Set<String> voted = new HashSet<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String args[]) {
         int select;
         do {
@@ -35,38 +36,38 @@ public class VotingSystem {
                 default:
                     System.out.println("Invalid choice");
             }
-        }while (select != 4);
-        }
-    private static void registerCandidate() {
-         System.out.println("Enter your name : ");
-         String candidateName=sc.nextLine();
-         if(candidate.containsKey(candidateName)){
-            System.out.println("already registered!");
-         }
-         else {
-            candidate.put(candidateName,0);
-            System.out.println("Register Success ");
-         }
+        } while (select != 4);
     }
-    private static void vote() {
 
+    private static void registerCandidate() {
+        System.out.println("Enter your name : ");
+        String candidateName = sc.nextLine();
+        if (candidate.containsKey(candidateName)) {
+            System.out.println("already registered!");
+        } else {
+            candidate.put(candidateName, 0);
+            System.out.println("Register Success ");
+        }
+    }
+
+    private static void vote() {
         System.out.println("Enter voter name : ");
         String name = sc.nextLine();
-         
-        if (candidate.containsKey(name)){
+        if (candidate.containsKey(name)) {
             System.out.println("voted success");
-        }
-        else{
+
+        } else {
             System.out.println("register first after voting ...");
         }
     }
+
     private static void showResult() {
         System.out.println("voting result");
-        for(Map.Entry<String, Integer> result : candidate.entrySet()){
+        for (Map.Entry<String, Integer> result : candidate.entrySet()) {
             String name = result.getKey();
             int voter = result.getValue();
-            System.out.println(name+ " : " + voter );
-        }
+            System.out.println(name + " : " + voter );
 
+        }
     }
 }
